30b9aed7baf55c2a23867a420eb08d4d8eee34a7
Varane1999 <harrowadewale@gmail.com>
2023-11-15 11:44:38 -0800

    builtin

---
+++ bgetLine.c
@@ -0,0 +1,170 @@
+#include "shell.h"
+
+/**
+ * input_buf - buffers chained commands
+ * @binfo: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
+ */
+ssize_t input_buf(binfo_t *binfo, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		/*bfree((void **)binfo->cmd_buf);*/
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _bgetline(binfo, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_bhistory_list(binfo, *buf, binfo->histcount++);
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				binfo->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
+/**
+ * get_input - gets a line minus the newline
+ * @binfo: parameter struct
+ *
+ * Return: bytes read
+ */
+ssize_t get_input(binfo_t *binfo)
+{
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(binfo->arg), *b;
+
+	_putchar(BBUF_FLUSH);
+	r = input_buf(binfo, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len)	/* we have commands left in the chain buffer */
+	{
+		j = i; /* init new iterator to current buf position */
+		b = buf + i; /* get pointer for return */
+
+		check_bchain(binfo, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
+		{
+			if (bchain(binfo, buf, &j))
+				break;
+			j++;
+		}
+
+		i = j + 1; /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			binfo->cmd_buf_type = BMD_NORM;
+		}
+
+		*buf_p = b; /* pass back pointer to current command position */
+		return (_strlen(b)); /* return length of current command */
+	}
+
+	*buf_p = buf; /* else not a chain, pass back buffer from _bgetline() */
+	return (r); /* return length of buffer from _bgetline() */
+}
+
+/**
+ * read_buf - reads a buffer
+ * @binfo: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t read_buf(binfo_t *binfo, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(binfo->readfd, buf, BBUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
+ * _bgetline - gets the next line of input from STDIN
+ * @binfo: parameter struct
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
+ *
+ * Return: s
+ */
+int _bgetline(binfo_t *binfo, char **ptr, size_t *length)
+{
+	static char buf[BBUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *b = NULL, *new_p = NULL, *c;
+
+	b = *ptr;
+	if (b && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(b, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (b ? free(b), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	b = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = b;
+	return (s);
+}
+
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BBUF_FLUSH);
+}
---
+++ builtin.c
@@ -0,0 +1,97 @@
+#include "boshell.h"
+
+/**
+ * _bexit - exits the shell
+ * @binfo: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
+ */
+int _bexit(binfo_t *binfo)
+{
+	int bexitcheck;
+
+	if (binfo->argv[1])  /* If there is an exit arguement */
+	{
+		bexitcheck = _berratoi(binfo->argv[1]);
+		if (bexitcheck == -1)
+		{
+			binfo->status = 2;
+			print_error(binfo, "Illegal number: ");
+			_bputs(info->argv[1]);
+			_bputchar('\n');
+			return (1);
+		}
+		binfo->err_num = _berratoi(binfo->argv[1]);
+		return (-2);
+	}
+	binfo->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _bcd - changes the current directory of the process
+ * @binfo: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _bcd(binfo_t *binfo)
+{
+	char *s, *bdir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!binfo->argv[1])
+	{
+		bdir = _bgetenv(binfo, "HOME=");
+		if (!bdir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((bdir = _bgetenv(binfo, "PWD=")) ? bdir : "/");
+		else
+			chdir_ret = chdir(bdir);
+	}
+	else if (_strcmp(binfo->argv[1], "-") == 0)
+	{
+		if (!_bgetenv(binfo, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_bgetenv(binfo, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((bdir = _bgetenv(binfo, "OLDPWD=")) ? bdir : "/");
+	}
+	else
+		chdir_ret = chdir(binfo->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(binfo, "can't cd to ");
+		_bputs(binfo->argv[1]), _bputchar('\n');
+	}
+	else
+	{
+		_bsetenv(binfo, "OLDPWD", _bgetenv(binfo, "PWD="));
+		_bsetenv(binfo, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _bhelp - changes the current directory of the process
+ * @binfo: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _bhelp(binfo_t *binfo)
+{
+	char **arg_array;
+
+	arg_array = binfo->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
